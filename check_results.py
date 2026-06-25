2bbbec88cb13c3b8d19abbfa8d2259a69bc84de5
Mondy Zy <mondyzy@MondydeMac-Studio.local>
2026-06-25 09:26:35 +0800

    Cleanup: remove temporary search scripts

--- check_results.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import json
-d = json.load(open('/tmp/s2_search_results.json'))
-for k, v in d.items():
-    print(f'{k}: {len(v)} papers')
-    for p in v:
-        print(f"  [{p.get('date','')[:10]}] {p.get('arxiv_id',''):15s} {p['title'][:70]}")
